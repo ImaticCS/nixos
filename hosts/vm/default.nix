2872694a7feb31165ee0ec3ff97719a412706007
imatic <imatic_public@protonmail.com>
2026-09-09 14:07:20 +1000

    vm: use upstream latest kernel

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -23,8 +23,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-zen4;
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-zen4;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
