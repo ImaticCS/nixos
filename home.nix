ada2bbd81bdb0754808a6a1ff64b8e8f8f4849e0
imatic <ImaticCS@users.noreply.github.com>
2026-09-17 15:52:48 +1000

    Cleanup home.nix

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, plasma-manager, lib, ... }:
+{ pkgs, plasma-manager, ... }:
 
 {
   imports = [
@@ -7,7 +7,6 @@
 
   home.username = "imatic";
   home.homeDirectory = "/home/imatic";
-
   home.stateVersion = "26.05";
 
   programs.home-manager.enable = true;
@@ -16,39 +15,11 @@
     htop
     fastfetch
   ];
-/*
-  home.sessionPath = [
-    "$HOME/.local/bin"
-  ];
-
-  home.activation.installYtDlpNightly =
-    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [ ! -x "$HOME/.local/bin/yt-dlp" ]; then
-        mkdir -p "$HOME/.local/bin"
-
-        tmp="$HOME/.local/bin/yt-dlp.tmp"
-        trap 'rm -f "$tmp"' EXIT
-
-        ${pkgs.curl}/bin/curl -fL \
-          https://github.com/yt-dlp/yt-dlp-nightly-builds/releases/latest/download/yt-dlp_linux \
-          -o "$tmp"
-
-        chmod +x "$tmp"
-        mv "$tmp" "$HOME/.local/bin/yt-dlp"
-      fi
-    '';
-*/
-  #xdg.configFile."klassy/klassyrc" = {
-  #  source = ./dotfiles/klassy/klassyrc;
-  #  force = true;
-  #};
 
   programs.plasma = {
     enable = true;
 
     workspace = {
-      #lookAndFeel = "org.kde.breezedark.desktop";
-      # org.kde.klassykitedarkbottompanel.desktop
 
       # Plasma Style
       theme = "breeze-dark";
@@ -85,32 +56,6 @@
           PopupMenus = 94;
         };
       };
-      /*
-            "klassy/klassyrc" = {
-              Global = {
-                #LookAndFeelSet = "org.kde.breezedark.desktop";
-                LookAndFeelSet = "org.kde.klassykitedarkbottompanel.desktop";
-                #RefreshedConfig = "6.5.3";
-              };
-
-              TitleBarOpacity = {
-                ActiveTitleBarOpacity = 80;
-                InactiveTitleBarOpacity = 64;
-              };
-
-              Windeco = {
-                ButtonIconStyle = "StyleFluent";
-                ColorizeWindowOutlineWithButton = false;
-                DrawTitleBarSeparator = false;
-                WindowCornerRadius = 12;
-              };
-
-              WindowOutlineStyle = {
-                WindowOutlineAccentColorOpacityActive = 30;
-                WindowOutlineStyleActive = "WindowOutlineAccentColor";
-              };
-            };
-      */
     };
   };
 }
